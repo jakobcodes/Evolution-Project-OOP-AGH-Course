c59d93396ceba818b1ab531b3c9f1c695363607b
jakobcodes <85311656+jakobcodes@users.noreply.github.com>
2021-10-21 17:41:41 +0200

    lab02 - code made at labs

---
+++ Vector2d.java
@@ -0,0 +1,68 @@
+package agh.ics.oop;
+
+import java.util.Vector;
+
+public class Vector2d {
+    public static void main(String[] args) {
+        Vector2d position1 = new Vector2d(1,2);
+        System.out.println(position1);
+        Vector2d position2 = new Vector2d(-2,1);
+        System.out.println(position2);
+        System.out.println(position1.add(position2));
+    }
+
+    public final int x;
+
+    public final int y;
+
+    Vector2d(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public String toString(){
+        return "(" + this.x + "," + this.y + ")";
+    }
+
+    boolean precedes(Vector2d other){
+        return this.x <= other.x && this.y <= other.y;
+    }
+
+    boolean follows(Vector2d other){
+        return this.x >= other.x && this.y >= other.y;
+    }
+
+    Vector2d upperRight(Vector2d other) {
+
+        int maxX = Math.max(this.x, other.x);
+
+        int maxY = Math.max(this.y, other.y);
+
+        return new Vector2d(maxX,maxY);
+    }
+
+    Vector2d lowerLeft(Vector2d other) {
+
+        int minX = Math.min(this.x, other.x);
+
+        int minY = Math.min(this.y, other.y);
+
+        return new Vector2d(minX,minY);
+    }
+
+    Vector2d add(Vector2d other) {
+        return new Vector2d(this.x + other.x, this.y + other.y);
+    }
+
+    Vector2d subtract(Vector2d other) {
+        return new Vector2d(this.x - other.x, this.y - other.y);
+    }
+
+//    public boolean equals(Object other){
+//
+//    }
+
+    Vector2d opposite(){
+        return new Vector2d(-this.x, - this.y);
+    }
+}
